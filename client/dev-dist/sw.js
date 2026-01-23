e00fc07c80e57549dcd5acece8c58beac783d161
nowonjae <nodnjs1231@quve.kr>
2026-01-23 14:39:12 +0900

    Upgrade : vercel agent skill
    vercel agent skill

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -83,7 +83,7 @@ define(['./workbox-cffa4085'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.l6jkh6bccp"
+    "revision": "0.v90mk2c0uro"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
