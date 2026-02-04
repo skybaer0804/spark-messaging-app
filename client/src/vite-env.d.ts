05426e98baba5f22195b2e3298aba1b56889cdc4
nowonjae <nodnjs1231@quve.kr>
2026-02-04 17:13:01 +0900

    Modify : vite-env.d.ts

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,8 +1,12 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_SOCKET_URL?: string;
-  readonly VITE_PROJECT_KEY?: string;
+  readonly VITE_API_URL: string;
+  readonly VITE_VAPID_PUBLIC_KEY: string;
+  readonly VITE_SPARK_SOCKET_URL: string;
+  readonly VITE_SPARK_PROJECT_KEY: string;
+  readonly VITE_MEETING_MAX_PARTICIPANTS?: string;
+  readonly VITE_MEETING_MAX_VIDEO_STREAMS?: string;
 }
 
 interface ImportMeta {
