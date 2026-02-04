9b578cb2a5382190bb25ab62abca59130862ecae
nowonjae <nodnjs1231@quve.kr>
2026-02-04 16:52:44 +0900

    Modify : test

--- client/src/config/sparkMessaging.ts
+++ client/src/config/sparkMessaging.ts
@@ -1,15 +1,18 @@
 import SparkMessaging from '@skybaer0804/spark-messaging-client';
 
-const serverUrl = import.meta.env.VITE_SPARK_SOCKET_URL || 'http://localhost:5000';
-const projectKey = import.meta.env.VITE_SPARK_PROJECT_KEY || 'default-project-key-12345';
+const serverUrl = import.meta.env.VITE_SPARK_SOCKET_URL;
+const projectKey = import.meta.env.VITE_SPARK_PROJECT_KEY;
+
+console.log('serverUrl', serverUrl);
+console.log('projectKey', projectKey);
 
 // SDK 생성자 호출 (생성자 오버로드 사용)
 const sparkMessagingClient = new SparkMessaging(serverUrl, projectKey);
 
 // 회의 제한 설정
 export const MEETING_CONFIG = {
-  MAX_PARTICIPANTS: parseInt(import.meta.env.VITE_MEETING_MAX_PARTICIPANTS || '10', 10),
-  MAX_VIDEO_STREAMS: parseInt(import.meta.env.VITE_MEETING_MAX_VIDEO_STREAMS || '4', 10),
+  MAX_PARTICIPANTS: parseInt(import.meta.env.VITE_MEETING_MAX_PARTICIPANTS || '2', 10),
+  MAX_VIDEO_STREAMS: parseInt(import.meta.env.VITE_MEETING_MAX_VIDEO_STREAMS || '2', 10),
 };
 
 export default sparkMessagingClient;
--- client/src/core/api/ApiService.ts
+++ client/src/core/api/ApiService.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 import { currentWorkspaceId } from '@/stores/chatRoomsStore';
 import { getLocalStorage, removeLocalStorage } from '@/core/utils/storageCache';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+// const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: API_URL,
